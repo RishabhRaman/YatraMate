4c1b4c5c8b0298040cf61537ba9370f502b6afe6
RishabhRaman <rishabhraman9@gmail.com>
2025-08-04 22:30:02 +0530

    added env of backend

--- frontend/src/services/aservice.js
+++ frontend/src/services/aservice.js
@@ -1,7 +1,7 @@
 // frontend/src/services/aservice.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/auth'; // Backend is running on port 3000
+const API_URL = import.meta.env.VITE_API_BASE_URL; // Backend is running on port 3000
 
 export const apiService = {
   register: async (fullName, email, password) => {
